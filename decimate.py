c68fe54f4ca2d4f42aa554d45c6841ea877827a8
jacobsela <jacob.sela@campus.technion.ac.il>
2019-10-28 12:13:43 +0200

    Add files via upload

---
+++ decimate.py
@@ -0,0 +1,14 @@
+#make a new folder with every ith frame
+
+import os
+import sys
+
+folder = sys.argv[1]
+factor = sys.argv[2]
+
+for i in range(len(os.listdir(folder))):
+	for filename in os.listdir(folder):
+	    if filename.endswith("{}.jpg".format(i)) or filename.endswith("{}.png".format(i)):
+	    	if i%int(factor) != 0:
+	    		os.remove (folder + "/" + filename)
+	    
